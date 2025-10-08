e985b761bc7a218ce1b0ff0be29b34bb6d99714c
dapacruz <davidcruz72@gmail.com>
2025-10-08 14:23:32 -0700

    Disable instrumentation hook for Edge Runtime compatibility

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Disable instrumentation for Edge Runtime compatibility
+  experimental: {
+    instrumentationHook: false,
+  },
+}
 
 module.exports = nextConfig
